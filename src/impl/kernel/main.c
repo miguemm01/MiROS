05367f53fd917d5b29e80f0c4979e7fb44a359d0
migue <mmm0015@alu.medac.es>
2021-02-24 13:09:04 +0100

    MiROS Kernel v0.1

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,9 @@
+#include "std-miros.h"
+
+void kernel_main(){
+    clearscr();
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+    printf("MiROS Kernel v0.1 - Welcome");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    printf("Written by Miguel Angel Moreno Martinez - @miiguemm01 on GitHub and Instagram");
+}
